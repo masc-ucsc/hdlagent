8626380e1b5ceee60937b72aabe7a77ee8f9b3e3
mzakharo <markzakharov1102@gmail.com>
2024-02-02 15:45:32 -0800

    small refactor of handler.py

--- hdlagent/handler.py
+++ hdlagent/handler.py
@@ -10,8 +10,19 @@ class Handler:
         self.lec_iter  = 1
         self.tb_iter   = 1
 
+        self.id        = None
+
+    # Assign one agent to handler
+    #
+    # Intended use: Initialization of Handler
     def set_agent(self, agent: Agent):
         self.agent = agent
+
+    # Parallel processing Handler enumeration
+    #
+    # Intended use: testing
+    def set_id(self, num: int):
+        self.id = num
 
     # Sets the amount of allowed iterations for a compile loop, which can be
     # nested within a LEC loop or a testbench loop. The maximum amount of queries
@@ -43,36 +54,33 @@ class Handler:
             exit()
         self.tb_iter = n
 
-    def json_run(self, json_file: str, limit: int = -1, use_spec: bool = False):
-        if json_file is None:
-            print("Error: no json file registered, exiting...")
-            exit()
+    def single_json_run(self, entry):
+            self.agent.set_module_name(entry['name'])
+            self.agent.set_pipeline_stages(int(entry['pipeline_stages']))
 
-        with open(json_file, 'r') as file:
-            data = json.load(file)['verilog_problems']
-
-        if limit == -1 or limit > len(data):
-            limit = len(data)
-
-        for i in range(limit):
-            entry        = data[i]
-            name         = entry['name']
-            prompt       = entry['instruction']
-            gold_verilog = entry['response']
-            pipe_stages  = int(entry['pipeline_stages'])
-
-            self.agent.set_module_name(name)
-            self.agent.set_pipeline_stages(pipe_stages)
-
-            if use_spec:
+            prompt = entry['instruction']
+            if self.agent.spec is not None:
                 if not self.agent.spec_exists():
                     self.agent.generate_spec(prompt)
                 prompt = self.agent.read_spec()
 
-            self.agent.dump_gold(gold_verilog)
+            self.agent.dump_gold(entry['response'])
             self.agent.lec_loop(prompt, self.lec_iter, self.comp_iter)
+        
+
+    def json_run(self, json_data, limit: int = -1):
+        if json_data is None:
+            print("Error: no json file registered, exiting...")
+            exit()
+
+        data = json_data['verilog_problems']
+        if limit == -1 or limit > len(data):
+            limit = len(data)
+
+        for i in range(limit):
+            self.single_json_run(data[i])
             
-    def entrypoint(self, spath: str, llm: str, lang: str, json_path: str = None, json_limit: int = -1, w_dir: str = './', use_spec: bool = False, init_context: bool = False, supp_context: bool = False):
+    def sequential_entrypoint(self, spath: str, llm: str, lang: str, json_path: str = None, json_limit: int = -1, w_dir: str = './', use_spec: bool = False, init_context: bool = False, supp_context: bool = False):
         self.set_agent(Agent(spath, llm, lang, init_context, supp_context, use_spec))
         self.agent.set_w_dir(w_dir)
 
@@ -84,8 +92,8 @@ class Handler:
             else:
                 try:
                     with open(json_path, 'r') as file:
-                        json.load(file)
+                        data = json.load(file)
                 except json.JSONDecodeError:
                     print("Error: json_path supplied is not a valid .json file, exiting...")
                     exit()
-                self.json_run(json_path, json_limit, use_spec)
+                self.json_run(data, json_limit)
