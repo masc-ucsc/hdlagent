f546f24fd062d6264de94e411945e186bbf0e86a
mzakharo <markzakharov1102@gmail.com>
2024-02-01 19:06:00 -0800

    cleanup directory structure

--- hdlagent/agent.py
+++ hdlagent/agent.py
@@ -3,10 +3,7 @@ import markdown
 import openai
 import os
 import octoai
-#from octoai.chat import TextModel
-#from octoai.client import Client
 import re
-#import replicate
 import subprocess
 import sys
 import time
@@ -14,25 +11,27 @@ import yaml
 
 from importlib import resources
 
-def list_openai_models():
+def list_openai_models(warn: bool = True):
     if "OPENAI_API_KEY" in os.environ:
         ret_list = []
         for entry in openai.Model.list()['data']:
             ret_list.append(entry['id'])
         return ret_list
     else:
-        print("OPENAI_API_KEY not set")
-        return None
+        if warn:
+            print("OPENAI_API_KEY not set")
+        return []
 
-def list_octoai_models():
+def list_octoai_models(warn: bool = True):
     if "OCTOAI_TOKEN" in os.environ:
         return octoai.chat.get_model_list()
     else:
-        print("OCTOAI_TOKEN not set")
-        return None
+        if warn:
+            print("OCTOAI_TOKEN not set")
+        return []
 
 class Agent:
-    def __init__(self, spath:str, model: str, lang: str, use_init_context:bool = False, use_supp_context: bool = False, use_spec: bool = False):
+    def __init__(self, spath: str, model: str, lang: str, use_init_context:bool = False, use_supp_context: bool = False, use_spec: bool = False):
 
         self.script_dir = spath
         # self.script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -85,9 +84,9 @@ class Agent:
         # Name to be specified in chat completion API of target LLM
         self.octoai_model = False
         self.openai_model = False
-        if model in list_octoai_models():
+        if model in list_octoai_models(False):
             self.octoai_model = True
-        elif model in list_openai_models():
+        elif model in list_openai_models(False):
             self.openai_model = True
         else:
             print("Error: invalid model, please check --list_openai_models or --list_octoai_models for available LLM selection, exiting...")
@@ -95,31 +94,33 @@ class Agent:
         self.model = model
 
         # Resources used as a part of conversational flow and performance counters
-        self.conversation      = []
-        self.name              = ""
-        self.pipe_stages       = 0
-        self.lec_script        = os.path.join(self.script_dir,"common/comb_lec")
-        self.tb_script         = os.path.join(self.script_dir,"common/iverilog_tb")
-        self.comp_n            = 0
-        self.comp_f            = 0
-        self.lec_n             = 0
-        self.lec_f             = 0
-        self.prompt_tokens     = 0
-        self.completion_tokens = 0
-        self.llm_query_time    = 0.0
-        self.world_clock_time  = 0.0
+        self.spec_conversation    = []
+        self.compile_conversation = []
+        self.name                 = ""
+        self.pipe_stages          = 0
+        self.lec_script           = os.path.join(self.script_dir,"common/comb_lec")
+        self.tb_script            = os.path.join(self.script_dir,"common/iverilog_tb")
+        self.comp_n               = 0
+        self.comp_f               = 0
+        self.lec_n                = 0
+        self.lec_f                = 0
+        self.prompt_tokens        = 0
+        self.completion_tokens    = 0
+        self.llm_query_time       = 0.0
+        self.world_clock_time     = 0.0
 
         # Where the resulting source files and logs will be managed and left
-        self.w_dir    = './'
-        self.log      = "log.md"
-        self.fail_log = "fail.md"
+        self.w_dir       = './'
+        self.spec_log    = "./logs/spec_log.md"
+        self.compile_log = "./logs/compile_log.md"
+        self.fail_log    = "./logs/fail.md"
         if use_spec:
-            self.spec = "spec.yaml"
+            self.spec    = "spec.yaml"
         else:
             self.spec = None
-        self.code     = "out" + self.file_ext
-        self.verilog  = "out.v"
-        self.tb       = "tb.v"
+        self.code        = "out" + self.file_ext
+        self.verilog     = "out.v"
+        self.tb          = "./tests/tb.v"
 
         # File path to golden verilog module used for Logical Equivalence Check
         self.gold = None
@@ -165,13 +166,14 @@ class Agent:
     #
     # Intended use: inside of 'set_w_dir' or 'set_module_name'
     def update_paths(self):
-        self.log      = os.path.join(self.w_dir, self.name + "_log.md")
-        self.fail_log = os.path.join(self.w_dir, self.name + "_fail.md")
+        self.compile_log  = os.path.join(self.w_dir, "logs", self.name + "_compile_log.md")
+        self.fail_log     = os.path.join(self.w_dir, "logs", self.name + "_fail.md")
         if self.spec is not None:
-            self.spec = os.path.join(self.w_dir, self.name + "_spec.yaml")
-        self.code     = os.path.join(self.w_dir, self.name + self.file_ext)
-        self.verilog  = os.path.join(self.w_dir, self.name + ".v")
-        self.tb       = os.path.join(self.w_dir, self.name + "_tb.v")
+            self.spec     = os.path.join(self.w_dir, self.name + "_spec.yaml")
+            self.spec_log = os.path.join(self.w_dir, "logs", self.name + "_spec_log.md")
+        self.code         = os.path.join(self.w_dir, self.name + self.file_ext)
+        self.verilog      = os.path.join(self.w_dir, self.name + ".v")
+        self.tb           = os.path.join(self.w_dir, "tests", self.name + "_tb.v")
 
     # Helper function to check if the spec exists or not
     #
@@ -179,7 +181,7 @@ class Agent:
     def spec_exists(self):
         return os.path.exists(self.spec)
 
-    # Helper function that extracts and reformatd information from spec yaml file
+    # Helper function that extracts and reformats information from spec yaml file
     # to create initial prompt(s) for compilation loop and testbench
     # loop (if applicable).
     #
@@ -312,21 +314,21 @@ class Agent:
     # the context by appending both query and response to the context history,
     # managed performance counters for token usage, time, etc...
     #
-    # Intended use: with any function that requires code to be written
-    def query_model(self, clarification: str):
+    # Intended use: primary interface with LLM
+    def query_model(self, conversation, clarification: str, compile_convo: bool = False):
         # Add a clarification message, continuing the conversation
-        self.conversation.append({
+        conversation.append({
             'role': 'user',
             'content': clarification
         })
-        print(clarification)
+        print("**User:**\n" + clarification)
         query_start_time = time.time()
 
         if self.octoai_model:
             client = octoai.client.Client()
             completion = client.chat.completions.create(
                 model=self.model,
-                messages=self.conversation,
+                messages=conversation,
                 max_tokens=8192,
                 presence_penalty=0,
                 temperature=0.1,
@@ -339,7 +341,7 @@ class Agent:
             # Use the OpenAI Completion API in a chat-based approach
             completion = openai.ChatCompletion.create(
                 model=self.model,  # Use the model we want for research
-                messages=self.conversation,
+                messages=conversation,
             )
             response = completion.choices[0].message['content']
             self.prompt_tokens += completion['usage']['prompt_tokens']
@@ -351,9 +353,9 @@ class Agent:
         self.llm_query_time += (time.time() - query_start_time)
 
         # Add the model's response to the messages list to keep the conversation context
-        if not self.used_supp_context:   # Explains it as user wrote the code
-            print(response)
-            self.conversation.append({
+        if not compile_convo or not self.used_supp_context:   # Explains it as user wrote the code
+            print("**Assistant**\n" + response)
+            conversation.append({
                 'role': 'assistant',
                 'content': response
             })
@@ -370,18 +372,20 @@ class Agent:
     #
     # Intended use: before the compile loop flow is initiated to bootstrap a spec doc
     def generate_spec(self, prompt: str):
-        self.reset_conversation()
+        self.reset_conversations()
         (description_req, interface_req) = self.get_spec_initial_instructions(prompt)
         spec_contents  = "description: |\n  \n"
-        description    = self.query_model(description_req)
+        description    = self.query_model(self.spec_conversation, description_req)
         description_lines = description.split('\n')
         indented_lines = ["  " + line for line in description_lines]
         description    = '\n'.join(indented_lines)
         spec_contents += description
         spec_contents += "\ninterface: |\n  \n  "
-        spec_contents += self.extract_codeblock(self.query_model(interface_req))
+        spec_contents += self.extract_codeblock(self.query_model(self.spec_conversation, interface_req))
+        os.makedirs(os.path.dirname(self.spec), exist_ok=True)
         with open(self.spec, 'w') as file:
             file.write(spec_contents)
+        self.dump_spec_conversation()
 
     # The entrypoint to the Supplemental Contexts through dictionary look-up.
     # Parses error-pattern-matching dictionary instantiated by language-specific *.yaml file
@@ -449,15 +453,13 @@ class Agent:
             return res_string
         return ""   #TODO: how to deal with logic mismatch??
 
-    # Sets conversation to 'step 0', a state where no RTL generation queries have
-    # been sent, only Initial Contexts if applied.
+    # Sets conversations to 'step 0', a state where no RTL generation queries have
+    # been sent, only Initial Contexts if applied for compile conversation.
     #
     # Intended use: before starting a new Agent run
-    def reset_conversation(self, compile_conv: bool = False):
-        if compile_conv:    # starting compile conversation
-            self.conversation = self.initial_contexts
-        else:
-            self.conversation = []
+    def reset_conversations(self):
+        self.compile_conversation = self.initial_contexts
+        self.spec_conversation    = []
 
     # Clears all performance counters kept by Agent
     #
@@ -480,7 +482,7 @@ class Agent:
     def compilation_loop(self, prompt: str, iterations: int = 1):
         current_query = self.get_compile_initial_instruction(prompt)
         for _ in range(iterations):
-            self.dump_code(self.query_model(current_query))
+            self.dump_code(self.query_model(self.compile_conversation, current_query))
             compile_out  = self.test_compile()
             print(compile_out)
             if compile_out != "":
@@ -490,7 +492,7 @@ class Agent:
         return False
 
     def lec_loop(self, prompt: str, lec_iterations: int = 1, compile_iterations: int = 1):
-        self.reset_conversation(True)
+        self.reset_conversations()
         self.reset_perf_counters()
         for i in range(lec_iterations):
             if self.compilation_loop(prompt, compile_iterations):
@@ -503,36 +505,56 @@ class Agent:
                     else:
                         prompt = self.get_lec_fail_instruction(lec_out)
                 else:
-                    self.dump_conversation()
+                    self.dump_compile_conversation()
                     return True
             else:
                 if i == (lec_iterations - 1):
                     self.dump_failure("compile failure\n" + self.test_compile())
-        self.dump_conversation()
+        self.dump_compile_conversation()
         return False
 
-    def dump_conversation(self):
-        start_idx  = 0
-        md_content = "# Conversation\n\n"
-        if self.used_init_context:
-            md_content += "**system:** Initial contexts were appended to this conversation\n\n"
-            start_idx   = len(self.initial_contexts) - 1
-        for entry in self.conversation[start_idx:]:
+    # Helper function that reads and formats an LLM conversation
+    # into a Markdown string
+    #
+    # Intended use: when dumping a conversation to a *_log.md file
+    def format_conversation(self, conversation, start_idx: int = 0):
+        md_content = ""
+        for entry in conversation[start_idx:]:
             if 'role' in entry and 'content' in entry:
                 role        = entry['role'].capitalize()
                 content     = entry['content']
-                md_content += f"**{role}:** {content}\n\n"
+                md_content += f"**{role}:**\n{content}\n\n"
+        return md_content
+
+    def dump_spec_conversation(self):
+        md_content  = "# Spec Conversation\n\n"
+        md_content += self.format_conversation(self.spec_conversation)
+        os.makedirs(os.path.dirname(self.spec_log), exist_ok=True)
+        with open(self.spec_log, 'w') as md_file:
+            md_file.write(md_content)
+
+    def dump_compile_conversation(self):
+        start_idx  = 0
+        md_content = "# Compile Conversation\n\n"
+        if self.used_init_context:
+            md_content += "**system:** Initial contexts were appended to this conversation\n\n"
+            start_idx   = len(self.initial_contexts) - 1
+        md_content += self.format_conversation(self.compile_conversation, start_idx)
         if self.used_supp_context:
             # Manually 'tack on' the passing file
             with open(self.code, 'r') as f:
                 last_code = f.read()
-            md_content += "**Assistant:** " + last_code + "\n\n"
+            md_content += "**Assistant:**\n" + last_code + "\n\n"
             print(last_code)
             f.close()
         md_content += self.report_statistics()
-        with open(self.log, 'w') as md_file:
+        os.makedirs(os.path.dirname(self.compile_log), exist_ok=True)
+        with open(self.compile_log, 'w') as md_file:
             md_file.write(md_content)
 
+    # Writes reason for *lec loop* failure to fail log
+    #
+    # Intended use: when lec_loop fails, mainly for benchmarking
     def dump_failure(self, reason: str):
         with open(self.fail_log, "w") as md_file:
             md_file.write(reason)
