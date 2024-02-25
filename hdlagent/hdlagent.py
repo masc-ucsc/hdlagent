c7f1e7b11f57eb4e9e5ebcd921f6268eab1089b3
mzakharo <markzakharov1102@gmail.com>
2024-02-24 22:28:42 -0800

    added skip_completed option to help with testing

--- hdlagent/hdlagent.py
+++ hdlagent/hdlagent.py
@@ -84,7 +84,7 @@ def worker(shared_list, shared_index, lock, spath: str, llm: str, lang: str, jso
 def parallel_run(spath: str, llm: str, lang: str, json_path: str, json_limit: int, w_dir: str, use_spec: bool, init_context: bool, supp_context: bool):
     pass
 
-def main(llm: str = None, lang: str = None, json_path: str = None, json_limit: int = -1, comp_limit: int = 4, lec_limit: int = 1, lec_feedback_limit: int = -1, top_k: int = 1, start_from: str = None, w_dir: str = './', use_spec: bool = False, init_context: bool = False, supp_context: bool = False, help: bool = False, openai_models_list: bool = False, octoai_models_list: bool = False, vertexai_models_list: bool = False):
+def main(llm: str = None, lang: str = None, json_path: str = None, json_limit: int = -1, comp_limit: int = 4, lec_limit: int = 1, lec_feedback_limit: int = -1, top_k: int = 1, start_from: str = None, w_dir: str = './', skip_completed: bool = False, use_spec: bool = False, init_context: bool = False, supp_context: bool = False, help: bool = False, openai_models_list: bool = False, octoai_models_list: bool = False, vertexai_models_list: bool = False):
     if (llm is not None) and (lang is not None) and (json_path is not None):
         spath      = resources.files('resources')
         my_handler = Handler()
@@ -92,7 +92,7 @@ def main(llm: str = None, lang: str = None, json_path: str = None, json_limit: i
         my_handler.set_lec_iter(lec_limit)
         my_handler.set_lec_feedback_limit(lec_feedback_limit)
         my_handler.set_k(top_k)
-        my_handler.sequential_entrypoint(spath, llm, lang, json_path, json_limit, start_from, w_dir, use_spec, init_context, supp_context)
+        my_handler.sequential_entrypoint(spath, llm, lang, json_path, json_limit, start_from, skip_completed, w_dir, use_spec, init_context, supp_context)
     elif openai_models_list:
         print(agent.list_openai_models())
     elif octoai_models_list:
