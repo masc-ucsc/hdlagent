ea84ec8f7bcd56052fdf0cf18bbe639d67b7064f
mzakharo <markzakharov1102@gmail.com>
2024-03-04 15:15:29 -0800

    refactor json data indexing

--- hdlagent/handler.py
+++ hdlagent/handler.py
@@ -2,6 +2,40 @@ from agent import Agent
 import json
 import os
 
+def check_json(json_path: str):
+    data = None
+    if (json_path is not None):
+        if not os.path.exists(json_path):
+            print("Error: json_path supplied does not exist, exiting...")
+            exit()
+        else:
+            try:
+                with open(json_path, 'r') as file:
+                    data = json.load(file)
+            except json.JSONDecodeError:
+                print("Error: json_path supplied is not a valid .json file, exiting...")
+                exit()
+    return data
+
+def set_json_bounds(json_data, limit: int = -1, start_from: str = None):
+    if json_data is None:
+        print("Error: no json file registered, exiting...")
+        exit()
+    data = json_data['verilog_problems']
+
+    start_idx = 0
+    if start_from is not None:
+        start_idx = next((i for i, entry in enumerate(data) if entry['name'] == start_from), None)
+        if start_idx is None:
+            print("Error: start_from .json entry was not found, exiting...")
+            exit()
+    limit += start_idx
+
+    if (limit < 1) or (limit > len(data)) or (limit < start_idx):
+        limit = len(data)
+
+    return data[start_idx: limit]
+
 class Handler:
     def __init__(self, agent: Agent = None):
         self.agent = agent
@@ -110,43 +144,17 @@ class Handler:
                 self.agent.incr_k()
         
 
-    def json_run(self, json_data, limit: int = -1, start_from: str = None, skip_completed: bool = False, skip_successful: bool = False):
-        if json_data is None:
-            print("Error: no json file registered, exiting...")
-            exit()
-        data = json_data['verilog_problems']
-
-        start_idx = 0
-        if start_from is not None:
-            start_idx = next((i for i, entry in enumerate(data) if entry['name'] == start_from), None)
-            if start_idx is None:
-                print("Error: start_from .json entry was not found, exiting...")
-                exit()
-        limit += start_idx
-
-        if (limit < 1) or (limit > len(data)) or (limit < start_idx):
-            limit = len(data)
-
+    def json_run(self, json_data, skip_completed: bool = False, skip_successful: bool = False):
         base_w_dir = self.agent.w_dir
-        for i in range(start_idx, limit):
-            if not (skip_completed and self.check_completion(data[i], base_w_dir)):
-                self.single_json_run(data[i], base_w_dir)
+        for entry in json_data:
+            if not (skip_completed and self.check_completion(entry, base_w_dir)):
+                self.single_json_run(entry, base_w_dir)
             
-    def sequential_entrypoint(self, spath: str, llm: str, lang: str, json_path: str = None, json_limit: int = -1, start_from: str = None, skip_completed: bool = False, skip_successful: bool = False, w_dir: str = './', use_spec: bool = False, init_context: bool = False, supp_context: bool = False, temperature: float = None):
+    def sequential_entrypoint(self, spath: str, llm: str, lang: str, json_data, skip_completed: bool = False, skip_successful: bool = False, w_dir: str = './', use_spec: bool = False, init_context: bool = False, supp_context: bool = False, temperature: float = None):
         self.set_agent(Agent(spath, llm, lang, init_context, supp_context, use_spec))
         self.agent.set_w_dir(w_dir)
         self.agent.set_model_temp(temperature)
 
         # Sanity test json file before starting anything
-        if (json_path is not None):
-            if not os.path.exists(json_path):
-                print("Error: json_path supplied does not exist, exiting...")
-                exit()
-            else:
-                try:
-                    with open(json_path, 'r') as file:
-                        data = json.load(file)
-                except json.JSONDecodeError:
-                    print("Error: json_path supplied is not a valid .json file, exiting...")
-                    exit()
-                self.json_run(data, json_limit, start_from, skip_completed, skip_successful)
+        #data = set_json_bounds(check_json(json_path), json_limit, start_from)
+        self.json_run(json_data, skip_completed, skip_successful)
