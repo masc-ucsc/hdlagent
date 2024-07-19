20123b0b7cb43fdebd8291b5c98bd54d28807260
Farzaneh Rabiey <farzane.rabiey@gmail.com>
2024-07-19 12:33:24 -0700

    updated help for becnh

--- hdlagent/cli_agent.py
+++ hdlagent/cli_agent.py
@@ -35,6 +35,29 @@ def bench(args):
         print("ERROR: Benchmarks can not invoke --skip_completed and --update at the same time")
         exit()
 
+    if args.limit:
+        print("limit: ")
+        exit()
+    if args.comp_limit:
+        print("comp_limit: ")
+        exit()
+
+    if args.lec_limit:
+        print("lec_limit: ")
+        exit()
+     
+    if args.lec_limit_feedback:
+        print("lec_limit_feedback: ")
+        exit()
+
+    if args.skip_successful:
+        print("skip_successful: ")
+        exit()
+
+    if args.spec:
+        print("spec: ")
+        exit()
+
     for f in args.bench_list:
         print(f"BENCHMARKING... {f}(to be added)")
 
@@ -167,8 +190,11 @@ def add_bench_command(subparsers):
     parser = subparsers.add_parser("bench", help="Benchmark the existing hdlagent setup", add_help=False)
     add_shared_args(parser)
     parser.add_argument('--skip_completed',        action="store_false", help='Skip generated already generated tests')
-    parser.add_argument("bench_list", nargs="+", help="List of files to clean")
-
+    parser.add_argument("bench_list", nargs="*", help="List of files to clean")
+    parser.add_argument('--bench', type=str, help='Path to the JSON file for benchmarking.')
+    parser.add_argument('--bench_limit', type=int, help='Allows to specify the number of tests to run in the benchmark JSON file.')
+    parser.add_argument('--bench_from', type=str, help='Allows to specify from which entry to try in the benchmark JSON file.')
+    parser.add_argument('--bench_spec', action='store_true', help='Create and use a spec when using benchmarking.')
     return parser
 
 def add_build_command(subparsers):
